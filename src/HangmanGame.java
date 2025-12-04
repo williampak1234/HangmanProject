19559fb80f9685cf5fc9cd349e92f029fc776709
William Pak <83974749+williampak1234@users.noreply.github.com>
2025-12-04 16:47:10 -0500

    Initial commit — Hangman Game

---
+++ src/HangmanGame.java
@@ -0,0 +1,269 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class HangmanGame {
+    private int category;
+    private int difficulty;
+    private int chancesLeft = 6;
+
+    private String incorrectGuesses = "";
+
+    private boolean gameIsWon = false;
+
+    private Random random = new Random();
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private char[] hiddenWordState;
+    private String userWordState;
+
+    private String[] carsEasy = {"Toyota", "Honda", "Tesla", "Ford", "Chevy", "Dodge", "BMW", "Mercedes"};
+    private String[] carsMedium = {"Lamborghini", "Ferrari", "Corvette", "Volkswagen", "Rolls Royce", "Maserati"};
+    private String[] carsHard = {"Pagani", "Bugatti", "Koenigsegg", "Alfa Romeo", "Viper"};
+
+    private String[] fastFoodEasy = {"McDonalds", "Burger King", "Wendys", "Chick Fil A", "KFC"};
+    private String[] fastFoodMedium = {"Chipotle", "Five Guys", "Popeyes", "Subway", "Jersey Mikes", "Dominoes"};
+    private String[] fastFoodHard = {"Jolibee", "Cinnabon", "White Castle", "Panda Express"};
+
+    private String[] gamesEasy = {"Mario", "Sonic", "Wii Sports", "Super Smash Bros", "Halo", "Call of Duty", "Angry Birds"};
+    private String[] gamesMedium = {"Overwatch", "Valorant", "CSGO", "Rainbow Six Siege", "Minecraft", "OSU", "GTA"};
+    private String[] gamesHard = {"Cyberpunk", "Red Dead Redemption", "Phasmaphobia", "Starcraft", "League of Legends", "Jetpack Joyride" };
+
+    private String[] proLangEasy = {"HTML", "Java", "C", "Python"};
+    private String[] proLangMedium = {"Rust", "C Plus Plus", "C Sharp", "JavaScript", "CSS"};
+    private String[] proLangHard = {"Ruby on Rails", "TypeScript", "Swift", "Go", "PHP"};
+
+    public HangmanGame(int category, int difficulty) {
+        this.category = category;
+        this.difficulty = difficulty;
+
+    }
+
+    // Returns a random word based on user's category and difficulty choice
+    private String returnWord() {
+        if (this.category==1) {
+            if (this.difficulty == 1) {
+                return carsEasy[random.nextInt(carsEasy.length)];
+            }
+            else if (this.difficulty == 2) {
+                return carsMedium[random.nextInt(carsMedium.length)];
+            }
+            else if (this.difficulty == 3) {
+                return carsHard[random.nextInt(carsHard.length)];
+            }
+        }
+
+        else if (this.category==2) {
+            if (this.difficulty == 1) {
+                return fastFoodEasy[random.nextInt(fastFoodEasy.length)];
+            }
+            else if (this.difficulty == 2) {
+                return fastFoodMedium[random.nextInt(fastFoodMedium.length)];
+            }
+            else if (this.difficulty == 3) {
+                return fastFoodHard[random.nextInt(fastFoodHard.length)];
+            }
+        }
+
+        else if (this.category==3) {
+            if (this.difficulty == 1) {
+                return gamesEasy[random.nextInt(gamesEasy.length)];
+            }
+            else if (this.difficulty == 2) {
+                return gamesMedium[random.nextInt(gamesMedium.length)];
+            }
+            else if (this.difficulty == 3) {
+                return gamesHard[random.nextInt(gamesHard.length)];
+            }
+        }
+
+        else if (this.category==4) {
+            if (this.difficulty == 1) {
+                return proLangEasy[random.nextInt(proLangEasy.length)];
+            }
+            else if (this.difficulty == 2) {
+                return proLangMedium[random.nextInt(proLangMedium.length)];
+            }
+            else if (this.difficulty == 3) {
+                return proLangHard[random.nextInt(proLangHard.length)];
+            }
+        }
+        return null;
+    }
+
+    // Print out incorrect guesses
+    private void incorrectGuesses(char userGuess) {
+
+        incorrectGuesses += userGuess + " ";
+        System.out.println();
+        System.out.println("Incorrect Guesses: " + incorrectGuesses);
+    }
+
+    // Print out guesses left
+    private void displayGuessesLeft() {
+
+        System.out.println("Chances left: " + chancesLeft);
+    }
+
+    // Draw the hangman picture based on how many chances user has left
+    private String drawHangman() {
+        if (chancesLeft == 6) {
+            return"""
+                      +---+
+                      |   |
+                      O   |
+                     /|\\  |
+                     / \\  |
+                          |
+                    =========""";
+        }
+        else if (chancesLeft == 5) {
+            return"""
+                      +---+
+                      |   |
+                      O   |
+                     /|\\  |
+                     /    |
+                          |
+                    =========""";
+
+        }
+        else if (chancesLeft == 4) {
+            return"""
+                      +---+
+                      |   |
+                      O   |
+                     /|\\  |
+                          |
+                          |
+                    =========""";
+
+        }
+        else if (chancesLeft == 3) {
+            return"""
+                      +---+
+                      |   |
+                      O   |
+                     /|   |
+                          |
+                          |
+                    =========""";
+
+        }
+        else if (chancesLeft == 2) {
+            return"""
+                      +---+
+                      |   |
+                      O   |
+                      |   |
+                          |
+                          |
+                    =========""";
+
+        }
+        else if (chancesLeft == 1) {
+            return"""
+                      +---+
+                      |   |
+                      O   |
+                          |
+                          |
+                          |
+                    =========""";
+
+        }
+        else if (chancesLeft == 0) {
+            return"""
+                      +---+
+                      |   |
+                          |
+                          |
+                          |
+                          |
+                    =========""";
+
+        }
+        else return "hello";
+    }
+
+
+    // Get user guess
+    private String getGuess() {
+        System.out.println("What letter is your guess?: ");
+        String userGuess = scanner.nextLine();
+        if (userGuess.length() == 1) {
+            return userGuess;
+        }
+        else {
+            while (userGuess.length() != 1) {
+                System.out.println("Please only enter 1 letter.");
+                System.out.println("What letter is your guess?: ");
+                userGuess = scanner.nextLine();
+            }
+        }
+        return userGuess;
+
+    }
+
+    // Check whether user guess was correct, do actions accordingly
+    private void checkGuess(String userGuess, String userWord) {
+        if (userWord.contains(userGuess)) {
+            char userGuessChar = userGuess.charAt(0);
+            System.out.println("You got a letter correct!");
+            for(int i = 0; i<userWord.length(); i++) {
+                if(userWord.charAt(i) == userGuessChar) {
+                    hiddenWordState[i] = userGuessChar;
+                }
+            }
+
+        }
+        else {
+            char userGuessChar = userGuess.charAt(0);
+            System.out.println("Wrong!");
+            chancesLeft--;
+            if (chancesLeft > 0) {
+                incorrectGuesses(userGuessChar);
+            }
+            else if (chancesLeft == 0) {
+                System.out.println("You lost!");
+
+            }
+        }
+
+    }
+
+    private void displayGame() {
+        displayGuessesLeft();
+        System.out.println(drawHangman());
+    }
+
+    private boolean checkIfWin(char[] hiddenWord, String userWord) {
+        userWordState = new String(hiddenWord);
+
+        if (userWordState.equals(userWord)) {
+            System.out.println("You Win!");
+            return true;
+        }
+        return false;
+    }
+
+
+    public void run() {
+        String userWord = returnWord();
+
+        hiddenWordState = new char[userWord.length()];
+        for (int i = 0; i < userWord.length(); i++) {
+            hiddenWordState[i] = '_';
+        }
+
+        while(chancesLeft > 0 && gameIsWon==false) {
+            displayGame();
+            System.out.println(hiddenWordState);
+            String userGuess = getGuess();
+            checkGuess(userGuess, userWord);
+            gameIsWon = checkIfWin(hiddenWordState, userWord);
+        }
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isPlaying = true;
+    public static boolean wantsToPlayAgain;
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static int printMenu() {
+        System.out.println("""
+                                Welcome to Hangman!
+                1) New Game
+                2) Exit""");
+        System.out.println("Please enter in the number of your choice: ");
+        int menuChoice = scanner.nextInt();
+        scanner.nextLine();
+        return menuChoice;
+    }
+
+    private static int chooseCategory() {
+        System.out.println("""
+                                Please choose a catergory:
+                1) Cars
+                2) Fast Food
+                3) Video Games
+                4) Programming Languages""");
+        System.out.println("Please enter in the number of your choice: ");
+        int categoryChoice = scanner.nextInt();
+        scanner.nextLine();
+        return categoryChoice;
+
+    }
+
+    private static int chooseDifficulty() {
+        System.out.println("""         
+                                Please choose a difficulty:
+                1) Easy
+                2) Medium
+                3) Hard""");
+        System.out.println("Please enter in the number of your choice: ");
+        int difficultyChoice = scanner.nextInt();
+        scanner.nextLine();
+        return difficultyChoice;
+    }
+
+    private static boolean menuInstructor(int userMenuChoice) {
+        if (userMenuChoice == 1) {
+            int categoryChoice = chooseCategory();
+            int difficultyChoice = chooseDifficulty();
+            createGameInstance(categoryChoice, difficultyChoice);
+            return true;
+        }
+        else if (userMenuChoice == 2) {
+            endGame();
+            return false;
+        }
+        return false;
+    }
+
+    private static void createGameInstance(int category, int difficulty) {
+        HangmanGame game = new HangmanGame(category, difficulty);
+        game.run();
+    }
+
+    private static void askAgain() {
+
+        System.out.println("Would you like to play again? Y/N: ");
+        String userOption = scanner.nextLine();
+        while(!userOption.equals("Y") && !userOption.equals("N")) {
+            System.out.println("Please enter only Y or N");
+            System.out.println("Would you like to play again? Y/N: ");
+            userOption = scanner.nextLine();
+        }
+
+        if (userOption.equals("Y")) {
+            wantsToPlayAgain = true;
+        }
+        else {
+            wantsToPlayAgain = false;
+        }
+
+    }
+
+    private static void endGame() {
+        System.out.println("Thanks so much for playing, have a good day!");
+    }
+
+    public static void main(String[] args) {
+
+
+        while(isPlaying) {
+            int userChoice = printMenu();
+            isPlaying = menuInstructor(userChoice);
+            askAgain();
+            if (!wantsToPlayAgain){
+                break;
+            }
+        }
+        endGame();
+    }
+}
+
