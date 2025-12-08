6bf000ce251340008a57dd5946e764b6dd30ed3f
William Pak <83974749+williampak1234@users.noreply.github.com>
2025-12-08 18:07:08 -0500

    Changed boolean==false into !boolean

--- src/HangmanGame.java
+++ src/HangmanGame.java
@@ -256,7 +256,7 @@ public class HangmanGame {
             hiddenWordState[i] = '_';
         }
 
-        while(chancesLeft > 0 && gameIsWon==false) {
+        while(chancesLeft > 0 && !gameIsWon) {
             displayGame();
             System.out.println(hiddenWordState);
             String userGuess = getGuess();
